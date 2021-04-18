fae8e73e35140fc6af111da9f57f3d87c8f4272b
ArthurFiorette <ArthurFiorette@users.noreply.github.com>
2021-04-18 16:36:30 +0000

    🎨 Prettified code

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,5 +1,5 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
-import { ObjectType, Field } from 'type-graphql';
+import { Field, ObjectType } from 'type-graphql';
 
 @ObjectType()
 @Entity()
