026ebffe3e310ad0366403d15036b337dacde282
Hazork <arthur.fiorette@gmail.com>
2021-04-18 16:57:43 -0300

    :wrench: Standartize tsconfig

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import theme from '../theme';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <ColorModeProvider
