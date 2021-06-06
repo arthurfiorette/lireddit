ef73778b7988ffa57770dbe3eb4f08eef4368298
ArthurFiorette <ArthurFiorette@users.noreply.github.com>
2021-06-06 23:43:17 +0000

    🎨 Prettified code

--- web/src/utils/urql/error-exchange.ts
+++ web/src/utils/urql/error-exchange.ts
@@ -2,15 +2,17 @@ import Router from 'next/router';
 import { Exchange } from 'urql';
 import { pipe, tap } from 'wonka';
 
-export const errorExchange: Exchange = ({ forward }) => (ops$) => {
-  return pipe(
-    forward(ops$),
-    tap(({ error }) => {
-      if (error) {
-        if (error.message == '[GraphQL] Not authenticated') {
-          Router.push('/login');
+export const errorExchange: Exchange =
+  ({ forward }) =>
+  (ops$) => {
+    return pipe(
+      forward(ops$),
+      tap(({ error }) => {
+        if (error) {
+          if (error.message == '[GraphQL] Not authenticated') {
+            Router.push('/login');
+          }
         }
-      }
-    })
-  );
-};
+      })
+    );
+  };
